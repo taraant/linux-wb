0b238efc0ab8dc346a07e355a4c2dc9989a7973f
Giulio Benetti <giulio.benetti@micronovasrl.com>
2020-05-17 23:56:10 +0200

    serial: 8250_dw: add em485 support
    Need to use rs485 transceiver so let's use existing em485 485 emulation
    layer on top of 8250.
    
    Add rs485_config callback to port and uses the standard em485 start and
    stop helpers.
    
    Signed-off-by: Giulio Benetti <giulio.benetti@micronovasrl.com>
    [moved to use newly added start/stop helpers]
    Signed-off-by: Heiko Stuebner <heiko.stuebner@theobroma-systems.com>

--- drivers/tty/serial/8250/8250_dw.c
+++ drivers/tty/serial/8250/8250_dw.c
@@ -527,6 +527,9 @@ static int dw8250_probe(struct platform_device *pdev)
 	p->dev		= dev;
 	p->set_ldisc	= dw8250_set_ldisc;
 	p->set_termios	= dw8250_set_termios;
+	p->rs485_config = serial8250_em485_config;
+	up->rs485_start_tx = serial8250_em485_start_tx;
+	up->rs485_stop_tx = serial8250_em485_stop_tx;
 
 	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
 	if (!data)
