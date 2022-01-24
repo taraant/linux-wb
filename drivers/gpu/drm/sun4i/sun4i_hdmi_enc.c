84abcde8dee5733ce4f53328141a3c769c882cd8
The-going <48602507+The-going@users.noreply.github.com>
2022-01-24 13:07:16 +0300

    drv:gpu:drm:sun4i: Add HDMI audio sun4i-hdmi encoder
    Add HDMI audio support for the sun4i-hdmi encoder, used on
    the older Allwinner chips - A10, A20, A31.
    
    Most of the code is based on the BSP implementation. In it
    dditional formats are supported (S20_3LE and S24_LE), however
    there where some problems with them and only S16_LE is left.
    
    Signed-off-by: Stefan Mavrodiev <stefan@olimex.com>

--- drivers/gpu/drm/sun4i/sun4i_hdmi_enc.c
+++ drivers/gpu/drm/sun4i/sun4i_hdmi_enc.c
@@ -29,6 +29,8 @@
 #include <drm/display/drm_hdmi_helper.h>
 #include <drm/display/drm_hdmi_state_helper.h>
 
+#include <sound/soc.h>
+
 #include "sun4i_backend.h"
 #include "sun4i_crtc.h"
 #include "sun4i_drv.h"
@@ -67,6 +69,11 @@ static void sun4i_hdmi_disable(struct drm_encoder *encoder,
 	u32 val;
 
 	DRM_DEBUG_DRIVER("Disabling the HDMI Output\n");
+
+#ifdef CONFIG_DRM_SUN4I_HDMI_AUDIO
+	if (hdmi->hdmi_audio)
+		sun4i_hdmi_audio_destroy(hdmi);
+#endif
 
 	val = readl(hdmi->base + SUN4I_HDMI_VID_CTRL_REG);
 	val &= ~SUN4I_HDMI_VID_CTRL_ENABLE;
@@ -155,6 +162,11 @@ static void sun4i_hdmi_enable(struct drm_encoder *encoder,
 		val |= SUN4I_HDMI_VID_CTRL_HDMI_MODE;
 
 	writel(val, hdmi->base + SUN4I_HDMI_VID_CTRL_REG);
+
+#ifdef CONFIG_DRM_SUN4I_HDMI_AUDIO
+	if (hdmi->hdmi_audio && sun4i_hdmi_audio_create(hdmi))
+		DRM_ERROR("Couldn't create the HDMI audio adapter\n");
+#endif
 }
 
 static const struct drm_encoder_helper_funcs sun4i_hdmi_helper_funcs = {
@@ -196,6 +208,10 @@ static int sun4i_hdmi_get_modes(struct drm_connector *connector)
 	edid = drm_get_edid(connector, hdmi->ddc_i2c ?: hdmi->i2c);
 	if (!edid)
 		return 0;
+
+#ifdef CONFIG_DRM_SUN4I_HDMI_AUDIO
+	hdmi->hdmi_audio = drm_detect_monitor_audio(edid);
+#endif
 
 	DRM_DEBUG_DRIVER("Monitor is %s monitor\n",
 			 connector->display_info.is_hdmi ? "an HDMI" : "a DVI");
