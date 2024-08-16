b7b39db888b01497639aba9e43441f6ba55f3511
chraac <chraac@gmail.com>
2024-08-16 16:44:41 +0800

    driver: allwinner h618 emac

--- include/linux/of_gpio.h
+++ include/linux/of_gpio.h
@@ -15,12 +15,30 @@
 #include <linux/gpio.h>		/* FIXME: Shouldn't be here */
 #include <linux/of.h>
 
+/*
+ * This is Linux-specific flags. By default controllers' and Linux' mapping
+ * match, but GPIO controllers are free to translate their own flags to
+ * Linux-specific in their .xlate callback. Though, 1:1 mapping is recommended.
+ */
+enum of_gpio_flags {
+	OF_GPIO_ACTIVE_LOW = 0x1,
+	OF_GPIO_SINGLE_ENDED = 0x2,
+	OF_GPIO_OPEN_DRAIN = 0x4,
+	OF_GPIO_TRANSITORY = 0x8,
+	OF_GPIO_PULL_UP = 0x10,
+	OF_GPIO_PULL_DOWN = 0x20,
+	OF_GPIO_PULL_DISABLE = 0x40,
+};
+
 struct device_node;
 
 #ifdef CONFIG_OF_GPIO
 
 extern int of_get_named_gpio(const struct device_node *np,
 			     const char *list_name, int index);
+
+extern int of_get_named_gpio_flags(const struct device_node *np,
+		const char *list_name, int index, enum of_gpio_flags *flags);
 
 #else /* CONFIG_OF_GPIO */
 
