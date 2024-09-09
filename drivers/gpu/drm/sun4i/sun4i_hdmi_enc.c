f16d2b0fb3d370277ebe2a925e0b5da80a4d93b7
The-going <48602507+The-going@users.noreply.github.com>
2024-09-09 14:40:49 +0300

    Revert "drm/sun4i: hdmi: switch to struct drm_edid"
    This reverts commit bd730c77fa37fe2dda4b6e23f6921ef8a9b1bb97.

--- drivers/gpu/drm/sun4i/sun4i_hdmi_enc.c
+++ drivers/gpu/drm/sun4i/sun4i_hdmi_enc.c
@@ -190,24 +190,20 @@ sun4i_hdmi_connector_clock_valid(const struct drm_connector *connector,
 static int sun4i_hdmi_get_modes(struct drm_connector *connector)
 {
 	struct sun4i_hdmi *hdmi = drm_connector_to_sun4i_hdmi(connector);
-	const struct drm_edid *drm_edid;
+	struct edid *edid;
 	int ret;
 
-	drm_edid = drm_edid_read_ddc(connector, hdmi->ddc_i2c ?: hdmi->i2c);
-
-	drm_edid_connector_update(connector, drm_edid);
-	cec_s_phys_addr(hdmi->cec_adap,
-			connector->display_info.source_physical_address, false);
-
-	if (!drm_edid)
+	edid = drm_get_edid(connector, hdmi->ddc_i2c ?: hdmi->i2c);
+	if (!edid)
 		return 0;
 
 	DRM_DEBUG_DRIVER("Monitor is %s monitor\n",
 			 connector->display_info.is_hdmi ? "an HDMI" : "a DVI");
 
-
-	ret = drm_edid_connector_add_modes(connector);
-	drm_edid_free(drm_edid);
+	drm_connector_update_edid_property(connector, edid);
+	cec_s_phys_addr_from_edid(hdmi->cec_adap, edid);
+	ret = drm_add_edid_modes(connector, edid);
+	kfree(edid);
 
 	return ret;
 }
